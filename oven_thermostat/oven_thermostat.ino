eda5d923b770e138dc55cb0cf9ef40960f56d26f
Alexey Zaytsev <alex.rv9rh@gmail.com>
2022-01-08 17:25:19 +0500

    Config for ATtiny13, turn off Serial

--- oven_thermostat/oven_thermostat.ino
+++ oven_thermostat/oven_thermostat.ino
@@ -1,20 +1,20 @@
 
 #include <util/delay.h>
 
-
+/*
 // Pin config for Arduino (ATmega328P):
 #define REGULATOR A0
 #define SENSOR A1
 #define HEATER 2
 #define FAULT_LED 3
+*/
 
-/*
 // Pin config for ATtiny13A:
 #define REGULATOR 3 // ADC3 on ATtiny13 (pin 2)
 #define SENSOR 2 // ADC2 on ATtiny13 (pin 3)
 #define HEATER 0 // PB0 on ATtiny13 (pin 5)
 #define FAULT_LED PB1 // PB1 on ATtiny13 (pin 6)
-*/
+
 
 #define REGULATOR_MIN 0
 #define REGULATOR_MAX 1023
@@ -42,37 +42,37 @@ void setup() {
     digitalWrite(HEATER, LOW);
     digitalWrite(FAULT_LED, LOW);
 
-    Serial.begin(9600);
-    Serial.println("ready");
+    // Serial.begin(9600);
+    // Serial.println("ready");
 }
 
 void loop() {
     int sensorValue = analogRead(SENSOR);
-    Serial.print("sensor: ");
-    Serial.print(sensorValue);
+    // Serial.print("sensor: ");
+    // Serial.print(sensorValue);
 
     if (sensorValue < SENSOR_FAULT) {
         digitalWrite(HEATER, LOW);
         digitalWrite(FAULT_LED, HIGH);
-        Serial.print(" Temp sensor fault!");
+        // Serial.print(" Temp sensor fault!");
     } else {
         digitalWrite(FAULT_LED, LOW);
 
         int regulatorValue = analogRead(REGULATOR);
-        Serial.print("  regulator: ");
-        Serial.print(regulatorValue);
+        // Serial.print("  regulator: ");
+        // Serial.print(regulatorValue);
 
         if (regulatorValue > 0) {
             long mappedRegulatorValue = mapToSensorValue(regulatorValue);
-            Serial.print("  mappedRegulator: ");
-            Serial.print(mappedRegulatorValue);
+            // Serial.print("  mappedRegulator: ");
+            // Serial.print(mappedRegulatorValue);
 
             if (sensorValue - HYSTERESIS >= mappedRegulatorValue) {
                 digitalWrite(HEATER, LOW);
-                Serial.print("  heater is off");
+                // Serial.print("  heater is off");
             } else if (sensorValue + HYSTERESIS <= mappedRegulatorValue) {
                 digitalWrite(HEATER, HIGH);
-                Serial.print("  heater is on");
+                // Serial.print("  heater is on");
             }
 
         } else {
@@ -80,6 +80,6 @@ void loop() {
         }
     }
 
-    Serial.println("");
+    // Serial.println("");
     _delay_ms(500);
 }
--- oven_thermostat_with_adjust_up_limit/oven_thermostat_with_adjust_up_limit.ino
+++ oven_thermostat_with_adjust_up_limit/oven_thermostat_with_adjust_up_limit.ino
@@ -1,27 +1,26 @@
 /*
     Дополнительно реализована возможность настройки верхней границы 
     температурного диапазона при помощи подстроечного резистора (UP_LIMIT_ADJUSTER).
-
 */
 
 #include <util/delay.h>
 
-
+/*
 // Pin config for Arduino (ATmega328P):
 #define REGULATOR A0
 #define SENSOR A1
 #define UP_LIMIT_ADJUSTER A2
 #define HEATER 2
 #define FAULT_LED 3
+*/
 
-/*
 // Pin config for ATtiny13A:
 #define REGULATOR 3 // ADC3 on ATtiny13 (pin 2)
 #define SENSOR 2 // ADC2 on ATtiny13 (pin 3)
 #define UP_LIMIT_ADJUSTER 1 // ADC1 on ATtiny13 (pin 7)
 #define HEATER 0 // PB0 on ATtiny13 (pin 5)
 #define FAULT_LED PB1 // PB1 on ATtiny13 (pin 6)
-*/
+
 
 // диапазон значений переменного резистора:
 #define VAR_RESISTOR_MIN 0
@@ -45,11 +44,11 @@ long mapAdjusterToSensorValue(long adjusterValue) {
 
 long mapToSensorValue(long regulatorValue) {
     int adjusterValue = analogRead(UP_LIMIT_ADJUSTER);
-    Serial.print("  adjuster: ");
-    Serial.print(adjusterValue);
+    // Serial.print("  adjuster: ");
+    // Serial.print(adjusterValue);
     long sensorMax = mapAdjusterToSensorValue(adjusterValue);
-    Serial.print("  sensorMax: ");
-    Serial.print(sensorMax);
+    // Serial.print("  sensorMax: ");
+    // Serial.print(sensorMax);
 
     long mapped = ((regulatorValue - VAR_RESISTOR_MIN) * (sensorMax - SENSOR_MIN) 
         / (VAR_RESISTOR_MAX - VAR_RESISTOR_MIN)) + SENSOR_MIN;
@@ -66,37 +65,37 @@ void setup() {
     digitalWrite(HEATER, LOW);
     digitalWrite(FAULT_LED, LOW);
 
-    Serial.begin(9600);
-    Serial.println("ready");
+    // Serial.begin(9600);
+    // Serial.println("ready");
 }
 
 void loop() {
     int sensorValue = analogRead(SENSOR);
-    Serial.print("sensor: ");
-    Serial.print(sensorValue);
+    // Serial.print("sensor: ");
+    // Serial.print(sensorValue);
 
     if (sensorValue < SENSOR_FAULT) {
         digitalWrite(HEATER, LOW);
         digitalWrite(FAULT_LED, HIGH);
-        Serial.print(" Temp sensor fault!");
+        // Serial.print(" Temp sensor fault!");
     } else {
         digitalWrite(FAULT_LED, LOW);
 
         int regulatorValue = analogRead(REGULATOR);
-        Serial.print("  regulator: ");
-        Serial.print(regulatorValue);
+        // Serial.print("  regulator: ");
+        // Serial.print(regulatorValue);
 
         if (regulatorValue > 0) {
             long mappedRegulatorValue = mapToSensorValue(regulatorValue);
-            Serial.print("  mappedRegulator: ");
-            Serial.print(mappedRegulatorValue);
+            // Serial.print("  mappedRegulator: ");
+            // Serial.print(mappedRegulatorValue);
 
             if (sensorValue - HYSTERESIS >= mappedRegulatorValue) {
                 digitalWrite(HEATER, LOW);
-                Serial.print("  heater is off");
+                // Serial.print("  heater is off");
             } else if (sensorValue + HYSTERESIS <= mappedRegulatorValue) {
                 digitalWrite(HEATER, HIGH);
-                Serial.print("  heater is on");
+                // Serial.print("  heater is on");
             }
 
         } else {
@@ -104,6 +103,6 @@ void loop() {
         }
     }
 
-    Serial.println("");
+    // Serial.println("");
     _delay_ms(500);
 }
