03eaf39e324ddf5addabb228f882d49220b58cf6
Alexey Zaytsev <alex.rv9rh@gmail.com>
2022-01-08 15:18:15 +0500

    Refactor oven_thermostat

--- oven_thermostat/oven_thermostat.ino
+++ oven_thermostat/oven_thermostat.ino
@@ -3,43 +3,43 @@
 
 
 // Pin config for Arduino (ATmega328P):
-#define VAR_RESISTOR A0
-#define THERMO_RESISTOR A1
-#define HEATER_VT 2
+#define REGULATOR A0
+#define SENSOR A1
+#define HEATER 2
 #define FAULT_LED 3
 
 /*
 // Pin config for ATtiny13A:
-#define VAR_RESISTOR 3 // ADC3 on ATtiny13 (pin 2)
-#define THERMO_RESISTOR 2 // ADC2 on ATtiny13 (pin 3)
-#define HEATER_VT 0 // PB0 on ATtiny13 (pin 5)
+#define REGULATOR 3 // ADC3 on ATtiny13 (pin 2)
+#define SENSOR 2 // ADC2 on ATtiny13 (pin 3)
+#define HEATER 0 // PB0 on ATtiny13 (pin 5)
 #define FAULT_LED PB1 // PB1 on ATtiny13 (pin 6)
 */
 
-#define VAR_RESIST_MIN 0
-#define VAR_RESIST_MAX 1023
+#define REGULATOR_MIN 0
+#define REGULATOR_MAX 1023
 
-#define THERMO_RESIST_FAULT 300 // ниже этого значения считаем терморезистор в обрыве
-#define THERMO_RESIST_MIN 540 // ~27 degrees Celsius
-// #define THERMO_RESIST_MAX 820 // ~70 degrees Celsius
-#define THERMO_RESIST_MAX 910 // ~100 degrees Celsius
+#define SENSOR_FAULT 300 // ниже этого значения считаем терморезистор в обрыве
+#define SENSOR_MIN 540 // ~27 degrees Celsius
+// #define SENSOR_MAX 820 // ~70 degrees Celsius
+#define SENSOR_MAX 910 // ~100 degrees Celsius
 
 #define HYSTERESIS 10
 
 
-long convertToThermoResist(long varResist) {
-    long thermoResist = ((varResist - VAR_RESIST_MIN) * (THERMO_RESIST_MAX - THERMO_RESIST_MIN) 
-        / (VAR_RESIST_MAX - VAR_RESIST_MIN)) + THERMO_RESIST_MIN;
-    return thermoResist;
+long mapToSensorValue(long regulatorValue) {
+    long mapped = ((regulatorValue - REGULATOR_MIN) * (SENSOR_MAX - SENSOR_MIN) 
+        / (REGULATOR_MAX - REGULATOR_MIN)) + SENSOR_MIN;
+    return mapped;
 }
 
 void setup() {
-    pinMode(VAR_RESISTOR, INPUT);
-    pinMode(THERMO_RESISTOR, INPUT);
-    pinMode(HEATER_VT, OUTPUT);
+    pinMode(REGULATOR, INPUT);
+    pinMode(SENSOR, INPUT);
+    pinMode(HEATER, OUTPUT);
     pinMode(FAULT_LED, OUTPUT);
 
-    digitalWrite(HEATER_VT, LOW);
+    digitalWrite(HEATER, LOW);
     digitalWrite(FAULT_LED, LOW);
 
     Serial.begin(9600);
@@ -47,36 +47,36 @@ void setup() {
 }
 
 void loop() {
-    int thermoResist = analogRead(THERMO_RESISTOR);
-    Serial.print("thermo: ");
-    Serial.print(thermoResist);
+    int sensorValue = analogRead(SENSOR);
+    Serial.print("temp: ");
+    Serial.print(sensorValue);
 
-    if (thermoResist < THERMO_RESIST_FAULT) {
-        digitalWrite(HEATER_VT, LOW);
+    if (sensorValue < SENSOR_FAULT) {
+        digitalWrite(HEATER, LOW);
         digitalWrite(FAULT_LED, HIGH);
-        Serial.print(" Thermo resistor fault!");
+        Serial.print(" Temp sensor fault!");
     } else {
         digitalWrite(FAULT_LED, LOW);
 
-        int varResist = analogRead(VAR_RESISTOR);
-        Serial.print("  var: ");
-        Serial.print(varResist);
+        int regulatorValue = analogRead(REGULATOR);
+        Serial.print("  regulator: ");
+        Serial.print(regulatorValue);
 
-        if (varResist > 0) {
-            long convertedVarResist = convertToThermoResist(varResist);
-            Serial.print("  converted: ");
-            Serial.print(convertedVarResist);
+        if (regulatorValue > 0) {
+            long mappedRegulatorValue = mapToSensorValue(regulatorValue);
+            Serial.print("  mapped: ");
+            Serial.print(mappedRegulatorValue);
 
-            if (thermoResist - HYSTERESIS >= convertedVarResist) {
-                digitalWrite(HEATER_VT, LOW);
+            if (sensorValue - HYSTERESIS >= mappedRegulatorValue) {
+                digitalWrite(HEATER, LOW);
                 Serial.print("  heater is off");
-            } else if (thermoResist + HYSTERESIS <= convertedVarResist) {
-                digitalWrite(HEATER_VT, HIGH);
+            } else if (sensorValue + HYSTERESIS <= mappedRegulatorValue) {
+                digitalWrite(HEATER, HIGH);
                 Serial.print("  heater is on");
             }
 
         } else {
-            digitalWrite(HEATER_VT, LOW);
+            digitalWrite(HEATER, LOW);
         }
     }
 
